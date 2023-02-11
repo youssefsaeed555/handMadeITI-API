091c10546cf83de5d927fed341070a8bc0493454
youssef saeed <yossefsaid555@gmail.com>
2023-02-12 00:29:56 +0200

    update and enahance code

--- services/userServices.js
+++ services/userServices.js
@@ -1,9 +1,10 @@
 const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/users");
+const generateToken = require("../utils/generateToken");
 
 exports.changePassword = asyncHandler(async (req, res, next) => {
-  await User.findOneAndUpdate(
+  const user = await User.findOneAndUpdate(
     req.user._id,
     {
       password: await bcrypt.hash(req.body.password, 12),
@@ -11,5 +12,8 @@ exports.changePassword = asyncHandler(async (req, res, next) => {
     },
     { new: true }
   );
-  return res.status(200).json({ message: "update successfully" });
+
+  //generate token
+  const token = generateToken(user._id);
+  return res.status(200).json({ message: "update successfully", token });
 });
