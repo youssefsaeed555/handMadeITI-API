6e9a380aa788fff0e57647396f5f9dfac222d55d
youssef saeed <yossefsaid555@gmail.com>
2023-02-20 10:09:11 +0200

    edit product validator

--- utils/validators/productValidator.js
+++ utils/validators/productValidator.js
@@ -1,88 +1,89 @@
 const { check } = require("express-validator");
 const validator = require("../../middleware/express_validator");
 // eslint-disable-next-line import/newline-after-import
-const Category = require('../../models/categoryModel');//waiting
+const Category = require("../../models/categoryModel"); //waiting
 exports.createProductValidator = [
   check("title")
-  .isLength({min:3})
-  .withMessage("must be at least 3 chars")
-  .notEmpty()
-  .withMessage('Product required'),
+    .isLength({ min: 3 })
+    .withMessage("must be at least 3 chars")
+    .notEmpty()
+    .withMessage("Product required"),
   check("description")
     .notEmpty()
     .withMessage("product description is required")
-    .isLength({min:20})
+    .isLength({ min: 20 })
     .withMessage("Too long description"),
   check("quantity")
     .notEmpty()
     .withMessage("Product quantity is required")
     .isNumeric()
-    .withMessage('Product quantity must be a number'),
-  check("sold")
-    .optional()
-    .isNumeric()
-    .withMessage("must be a number"),
+    .withMessage("Product quantity must be a number"),
+  check("sold").optional().isNumeric().withMessage("must be a number"),
   check("price")
     .notEmpty()
     .withMessage("Product price is required")
     .isNumeric()
     .withMessage("Product price must be a number")
-    .isLength({max:32})
-    .withMessage('To long price'),
+    .isLength({ max: 32 })
+    .withMessage("To long price"),
   check("priceAfterDiscount")
     .optional()
     .isNumeric()
     .withMessage("Product priceAfterDiscount must be a number")
     .toFloat()
-    .custom((value,{req})=>{
-        if(req.body.price <= value){
-            throw new Error('priceAfterDiscount must be lower than price');
-        }
-        return true;
+    .custom((value, { req }) => {
+      if (req.body.price <= value) {
+        throw new Error("priceAfterDiscount must be lower than price");
+      }
+      return true;
     }),
   check("colors")
     .optional()
     .isArray()
     .withMessage("Colors must be array of string"),
-  check('images').optional().isArray().withMessage('images should be array of string'),
-   check('category')
-     .notEmpty()
-    .withMessage('Product must be belong to a category')
+  check("images")
+    .optional()
+    .isArray()
+    .withMessage("images should be array of string"),
+  check("category")
+    .notEmpty()
+    .withMessage("Product must be belong to a category")
     .isMongoId()
-    .withMessage('Invalid ID format ')
-      //check if this category exist or not
-    .custom((categoryId)=>Category.findById(categoryId).then((category)=>{
-     if(!category){
-       return Promise.reject(new Error(`No category for this id ${categoryId}`));
-     }
-    })),
-  check('ratingsAverage')
-   .optional()
-   .isNumeric()
-   .withMessage('rating must be a number')
-   .isLength({min:1})
-   .withMessage('Rating must be above or equal 1.0')
-   .isLength({max:5})
-   .withMessage('Rating must be below or equal 5.0'),
-  check('ratingQuantity')
-   .optional()
-   .isNumeric()
-   .withMessage('Rating Quantity must be a number '),
+    .withMessage("Invalid ID format ")
+    .custom(async (val, { req }) => {
+      const findCategory = await Category.findById(val);
+      if (!findCategory) {
+        throw new Error(`category for this id: ${val} not found`);
+      }
+      return true;
+    }),
+  check("ratingsAverage")
+    .optional()
+    .isNumeric()
+    .withMessage("rating must be a number")
+    .isLength({ min: 1 })
+    .withMessage("Rating must be above or equal 1.0")
+    .isLength({ max: 5 })
+    .withMessage("Rating must be below or equal 5.0"),
+  check("ratingQuantity")
+    .optional()
+    .isNumeric()
+    .withMessage("Rating Quantity must be a number "),
 
   validator,
 ];
 
-exports.getProductValidator= [
-    check('id').isMongoId().withMessage('Invalid ID format'),
-    validator,
+exports.getProductValidator = [
+  check("id").isMongoId().withMessage("Invalid ID format"),
+  validator,
 ];
 
-exports.updateProductValidator= [
-    check('id').isMongoId().withMessage('Invalid ID format'),
-    validator,
+exports.updateProductValidator = [
+  check("id").isMongoId().withMessage("Invalid ID format"),
+  validator,
 ];
 
-exports.deleteProductValidator=[
-    check('id').isMongoId().withMessage('Invalid ID format'),
-    validator,
+exports.deleteProductValidator = [
+  check("id").isMongoId().withMessage("Invalid ID format"),
+  validator,
 ];
